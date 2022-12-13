c78d24d3f9fade280714b106b5721f06c71f0b38
Ruslan <sonikx94@yandex.ru>
2022-12-13 21:10:07 +0300

    Добавил фон

--- gui/core/src/com/mygdx/game/MyGdxGame.java
+++ gui/core/src/com/mygdx/game/MyGdxGame.java
@@ -1,31 +1,32 @@
 package com.mygdx.game;
 
 import com.badlogic.gdx.ApplicationAdapter;
+import com.badlogic.gdx.Gdx;
 import com.badlogic.gdx.graphics.Texture;
 import com.badlogic.gdx.graphics.g2d.SpriteBatch;
 import com.badlogic.gdx.utils.ScreenUtils;
 
 public class MyGdxGame extends ApplicationAdapter {
-	SpriteBatch batch;
-	Texture img;
+	private SpriteBatch batch;
+	private Texture background;
 	
 	@Override
 	public void create () {
 		batch = new SpriteBatch();
-		img = new Texture("badlogic.jpg");
+		background = new Texture("background.png");
 	}
 
 	@Override
 	public void render () {
-		ScreenUtils.clear(1, 0, 0, 1);
+		ScreenUtils.clear(0, 0, 0, 1);
 		batch.begin();
-		batch.draw(img, 0, 0);
+		batch.draw(background, 0, 0, Gdx.graphics.getWidth(), Gdx.graphics.getHeight());
 		batch.end();
 	}
 	
 	@Override
 	public void dispose () {
 		batch.dispose();
-		img.dispose();
+		background.dispose();
 	}
 }
--- gui/desktop/src/com/mygdx/game/DesktopLauncher.java
+++ gui/desktop/src/com/mygdx/game/DesktopLauncher.java
@@ -10,7 +10,7 @@ public class DesktopLauncher {
 		Lwjgl3ApplicationConfiguration config = new Lwjgl3ApplicationConfiguration();
 		config.setForegroundFPS(60);
 		config.setTitle("Java Heroes");
-		config.setWindowedMode(1280, 720);
+		config.setWindowedMode(1024, 640);
 		config.setResizable(false);
 		new Lwjgl3Application(new MyGdxGame(), config);
 	}
